854c223181cb8e60dae366b840ab69d90437416f
Your Name <peejaygmulbah231@email.com>
2026-06-09 08:15:27 +0000

    update to live Render URL

--- extension/content.js
+++ extension/content.js
@@ -30,7 +30,7 @@ btn.addEventListener('click', async () => {
   const body = bodyEl ? bodyEl.innerText : '';
 
   try {
-    const response = await fetch('http://127.0.0.1:8000/analyze', {
+    const response = await fetch('https://phishguard-rdrv.onrender.com/analyze', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ sender, subject, body })
